29cd7a79f987e7d6c2fd6c873a7f48910542dba4
DeathG7n <christariccykid55@gmail.com>
2022-04-20 14:06:56 +0100

    First commit

--- ToDoApp/index.js
+++ ToDoApp/index.js
@@ -2,7 +2,6 @@ const chores = document.getElementById('input-el')
 const inputBtn = document.getElementById('btn')
 let ulEl = document.getElementById('ul-el')
 
-
 inputBtn.addEventListener("click", function(){
      let list = document.createElement("li")
      list.innerText = chores.value
@@ -10,10 +9,19 @@ inputBtn.addEventListener("click", function(){
      chores.value = ""
 
   list.addEventListener("click", function(){
+    let input = document.createElement("input")
+    chores.setAttribute("placeholder", "Enter new chore!!!")
+    input.setAttribute("type", "text")
+    input.setAttribute("value", chores.value) 
+    list.innerHTML = input.value
+    chores.value = ""
+   })
+  list.addEventListener("dblclick", function(){
     list.style.textDecoration = "line-through"
   })
-  list.addEventListener("dblclick", function(){
-    ulEl.removeChild(list)
-  })
-  
+  list.addEventListener('click', function (evt) {
+    if (evt.detail === 3) {
+      ulEl.removeChild(list)
+    }
+});
 })
